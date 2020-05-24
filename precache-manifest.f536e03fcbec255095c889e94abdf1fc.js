e9e97cca53187001482823694c9ece1e8a336465
WincherZ <wincher2016@gmail.com>
2020-05-24 19:51:03 +0300

    Updates

---
+++ precache-manifest.f536e03fcbec255095c889e94abdf1fc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c197f3056291e8c2ffea010bd2d858f8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d13ac0a2395c0be3a27c",
+    "url": "./static/css/main.6a2f1ffa.chunk.css"
+  },
+  {
+    "revision": "ab19883d9a3d31f729bb",
+    "url": "./static/js/2.3f67f8b4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.3f67f8b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d13ac0a2395c0be3a27c",
+    "url": "./static/js/main.5405f501.chunk.js"
+  },
+  {
+    "revision": "13d57103233adbc95222",
+    "url": "./static/js/runtime-main.7c3e9311.js"
+  },
+  {
+    "revision": "482e37260cd688576287f1976e958e0b",
+    "url": "./static/media/Mont-Bold.482e3726.otf"
+  },
+  {
+    "revision": "f0f845588eef439ac6ecc9b6ab42191c",
+    "url": "./static/media/Mont-Regular.f0f84558.otf"
+  },
+  {
+    "revision": "d41a520b1802421f24e21279736e1871",
+    "url": "./static/media/Mont-SemiBold.d41a520b.otf"
+  }
+]);
